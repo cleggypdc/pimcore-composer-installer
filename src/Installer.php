15980674f71ce0c74f7dbaddab0aafb4014e20af
Paul Clegg <paul@gatherdigital.co.uk>
2018-02-27 13:02:07 +0000

    initial commit

---
+++ src/Installer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Cleggypdc\Pimcore\Composer;
+
+use Composer\Script\Event;
+
+class Installer
+{
+
+    public static function install(Event $event)
+    {
+        var_dump($event);exit;
+    }
+
+}
